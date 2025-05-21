75f6b3dbaae2088c6c2ad14baa0b32e61b260784
Abhinav Venkat Sivakumar Gayathiri <Asivakumargayat1253@conestogac.on.ca>
2025-05-20 21:53:15 -0400

    Implemented Music (PlaySound)

--- helloworld/Source.c
+++ helloworld/Source.c
@@ -1,64 +1,86 @@
 #include "raylib.h"
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX_FILEPATH_RECORDED 4096
-#define MAX_FILE_SIZE		  2048
 
-//int main(void)
-//{
-//    InitAudioDevice();
-//
-//    const char* filename = "C:\\Users\\abhianu\\OneDrive\\Desktop\\helloworld\\assests\\Hallofmountainking.wav";
-//
-//    if (!FileExists(filename)) {
-//        printf("ERROR: File not found at path: %s\n", filename);
-//        return 1;
-//    }
-//
-//    Sound sound = LoadSound(filename);
-//
-//    if (sound.frameCount == 0) {
-//        printf("ERROR: Sound failed to load (0 frames).\n");
-//        return 1;
-//    }
-//
-//    printf("SUCCESS: Sound loaded and playing...\n");
-//    PlaySound(sound);
-//
-//    while (IsSoundPlaying(sound)) {
-//         Wait
-//    }
-//
-//    UnloadSound(sound);
-//    CloseAudioDevice();
-//
-//    return 0;
-//}
-
+#define MAX_FILEPATH_RECORDED 2
+#define MAX_FILE_SIZE 1024
 
 int main(void) {
+    const int w_width = 800;
+    const int w_height = 450;
 
-	const int w_height = 800;
-	const int w_width = 450;
+    InitWindow(w_width, w_height, "Noise Cancellation Program");
 
-	InitWindow(w_height, w_width, "Noise Cancellation Program");
+    int filepathCounter = 0;
+    char* filepath[MAX_FILEPATH_RECORDED] = { 0 };
 
-	int filepathCounter = 0;
-	char* filepath[MAX_FILEPATH_RECORDED] = { 0 };
+    for (int i = 0; i < MAX_FILEPATH_RECORDED; i++) {
+        filepath[i] = (char*)RL_CALLOC(MAX_FILE_SIZE, 1);
+    }
 
-	for (int i = 0; i < MAX_FILEPATH_RECORDED;i++) {
-		filepath[i] = (char*)RL_CALLOC(MAX_FILE_SIZE, 1);
-	}
-	SetTargetFPS(60);
-	while (!WindowShouldClose()) {
-		if (IsFileDropped()) {
-			FilePathList droppedFile = LoadDroppedFiles();
-			for (int i = 0, offset = filepathCounter; i < (int)droppedFile.count;i++) {
-				if (filepathCounter < (MAX_FILEPATH_RECORDED - 1)) {
-					TextCopy(filepath[offset + i], droppedFile.paths[i]);
-					filepathCounter++;
-				}
-			}
-		}
-	}
+    InitAudioDevice();
+
+    SetTargetFPS(60);
+
+    Sound sound = { 0 };
+    bool soundLoaded = false;
+
+    while (!WindowShouldClose()) {
+        if (IsFileDropped()) {
+            FilePathList droppedFiles = LoadDroppedFiles();
+
+            if (filepathCounter >= MAX_FILEPATH_RECORDED) {
+                printf("Too many files dropped.\n");
+            }
+            else {
+                
+                TextCopy(filepath[filepathCounter], droppedFiles.paths[0]);
+
+                if (FileExists(filepath[filepathCounter])) {
+                    sound = LoadSound(filepath[filepathCounter]);
+
+                    if (sound.frameCount == 0) {
+                        printf("ERROR: Failed to load sound from %s\n", filepath[filepathCounter]);
+                    }
+                    else {
+                        printf("SUCCESS: Loaded sound: %s\n", filepath[filepathCounter]);
+                        PlaySound(sound);
+                        soundLoaded = true;
+                    }
+
+                    filepathCounter++;
+                }
+                else {
+                    printf("ERROR: File does not exist: %s\n", filepath[filepathCounter]);
+                }
+            }
+
+            UnloadDroppedFiles(droppedFiles);
+        }
+
+        BeginDrawing();
+        ClearBackground(LIGHTGRAY);
+
+        if (filepathCounter == 0) {
+            DrawText("Drag and drop your music file here", 100, 40, 20, BLACK);
+        }
+        else {
+            DrawText("File dropped. Playing sound...", 100, 40, 20, BLACK);
+        }
+
+        EndDrawing();
+    }
+
+    
+    if (soundLoaded) {
+        UnloadSound(sound);
+    }
+
+    CloseAudioDevice();
+
+    for (int i = 0; i < MAX_FILEPATH_RECORDED; i++) {
+        RL_FREE(filepath[i]);
+    }
+
+    CloseWindow();
+    return 0;
 }
